a1466b32df6108a272bb25f3e641535d43f5e430
Aung <aungloo@gmail.com>
2013-11-26 00:26:10 -0800

    update aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
+set -U EDITOR vim
+
 . ~/.config/fish/fish_prompt.fish
 . ~/.config/fish/aliases.fish
+
+if test -f ~/.config/fish/local.fish
+  # configs local to machine or not applicable to all machines
+  # Share it with Dropbox
+  # ln -s ~/Dropbox/fish/machine1.fish ~/.config/fish/local.fish
+  . ~/.config/fish/local.fish
+end
 
