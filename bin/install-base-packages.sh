7e8338f950128f02582e4ca12299af3e994e584c
aung <aung@cooler-u>
2014-05-11 17:42:06 -0700

    update/add ubuntu install scripts

---
+++ bin/install-base-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y vim xclip
+sudo apt-get install -y openssh-server
+sudo apt-get install -y wget curl tree colordiff
+sudo apt-get install -y htop atop
+sudo apt-get install -y git
