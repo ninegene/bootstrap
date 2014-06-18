f2a92b15dd0bc7b65665e6d9cb2110b9179c6d57
aung <aung@dev1>
2014-06-18 14:14:08 -0700

    add more base packages

--- bin/install-base-packages.sh
+++ bin/install-base-packages.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python-software-properties
-sudo apt-get install -y vim xclip
+sudo apt-get install -y python-software-properties python-dev
+sudo apt-get install -y git vim xclip
 sudo apt-get install -y openssh-server
 sudo apt-get install -y wget curl tree colordiff
-sudo apt-get install -y htop atop
-sudo apt-get install -y git
+sudo apt-get install -y htop atop rsync zip unzip gzip bzip2
