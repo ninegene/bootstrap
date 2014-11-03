c0a6939b46c4915e4ea1eb2a25370f437e4f2114
aung <aung@macdev>
2014-11-02 19:03:09 -0800

    update to new git prompt

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,40 +1,46 @@
-# name: GitStatus
-# Find latest version from: https://github.com/godfat/fish_prompt-gitstatus
+# Informative Git Prompt from fish_config
 
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
 
-function _git_status_symbol
-  set -l git_status (git status --porcelain ^/dev/null)
-  if test -n "$git_status"
-    # Is there anyway to preserve newlines so we can reuse $git_status?
-    if git status --porcelain ^/dev/null | grep '^.[^ ]' >/dev/null
-      echo '*' # dirty
-    else
-      echo '#' # all staged
-    end
-  else
-    echo    '' # clean
-  end
-end
+set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
 
-function _remote_hostname
-  if test -n "$SSH_CONNECTION"
-    echo (whoami)@(hostname)
-  end
-end
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
 
-function fish_prompt
-  set -l cyan (set_color cyan)
-  set -l normal (set_color normal)
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green bold
 
-  set -l cwd (set_color $fish_color_cwd)(prompt_pwd)
-  set -l git_status (_git_status_symbol)(_git_branch_name)
 
-  if test -n "$git_status"
-    set git_status " $git_status"
+function fish_prompt --description 'Write out the prompt'
+
+  set -l last_status $status
+
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
   end
 
-  echo -n (_remote_hostname) $cwd$cyan$git_status$normal'> '
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  if not test $last_status -eq 0
+  set_color $fish_color_error
+  end
+
+  echo -n '$ '
+
 end
