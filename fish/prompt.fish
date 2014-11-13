9dd946041d42723e721250257d7be737ec421073
aung <aung@dev1>
2014-11-12 16:26:30 -0800

    update prompt to better git status and root user symbol

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,18 +1,18 @@
-# Informative Git Prompt from fish_config
+# Based on Git Prompt from fish_config, 'type fish_prompt'
 
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_hide_untrackedfiles 1
 
 set -g __fish_git_prompt_color_branch magenta bold
 set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_ahead "↑ "
+set -g __fish_git_prompt_char_upstream_behind "↓ "
 set -g __fish_git_prompt_char_upstream_prefix ""
 
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_stagedstate "● "
+set -g __fish_git_prompt_char_dirtystate "✚ "
 set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_conflictedstate "✖ "
 set -g __fish_git_prompt_char_cleanstate "✔"
 
 set -g __fish_git_prompt_color_dirtystate blue
@@ -21,26 +21,56 @@ set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 set -g __fish_git_prompt_color_cleanstate green bold
 
+function _remote_hostname
+  if test -n "$SSH_CONNECTION"
+    echo "$USER@$__fish_prompt_hostname "
+  end
+end
+
+function _git_status
+  set -l git_status (__fish_git_prompt)
+  if test -n "$git_status"
+    echo "$git_status"
+  end
+end
 
 function fish_prompt --description 'Write out the prompt'
 
-  set -l last_status $status
+  if not set -q __fish_prompt_hostname
+      set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
 
   if not set -q __fish_prompt_normal
     set -g __fish_prompt_normal (set_color normal)
   end
 
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
-
-  printf '%s ' (__fish_git_prompt)
-
-  if not test $last_status -eq 0
-  set_color $fish_color_error
+  if not set -q __fish_prompt_cyan
+    set -g __fish_prompt_cyan (set_color cyan)
   end
 
-  echo -n '$ '
+  set -l symbol '$'
+  switch $USER
+
+    case root
+
+      if not set -q __fish_prompt_cwd
+        if set -q fish_color_cwd_root
+          set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+        else
+          set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+      end
+
+      set symbol '#'
+
+    case '*'
+
+        if not set -q __fish_prompt_cwd
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+
+    end
+
+  echo -n -s (_remote_hostname) "$__fish_prompt_cwd" (prompt_pwd) (_git_status) "$__fish_prompt_normal" "$symbol "
 
 end
