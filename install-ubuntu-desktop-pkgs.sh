312d68477f923663c36bd9fa2038b5db82dfc682
aung <aung@cu>
2016-04-11 20:53:31 -0700

    desktop app: synapse

--- install-ubuntu-desktop-pkgs.sh
+++ install-ubuntu-desktop-pkgs.sh
@@ -6,3 +6,7 @@ sudo apt-get install -y gufw
 sudo apt-get install -y ttf-mscorefonts-installer
 sudo apt-get install -y chromium-browser
 sudo apt-get install -y xclip gitg gitk diffuse nautilus-open-terminal
+
+sudo add-apt-repository ppa:synapse-core/ppa
+sudo apt-get update
+sudo apt-get install synapse
