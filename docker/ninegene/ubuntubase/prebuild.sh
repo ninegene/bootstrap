f1499ca131b41b4cd6e1e3787194c5b9f09ad588
gene <gene@app1>
2017-10-14 00:18:44 -0700

    remove old dockerfile

--- docker/ninegene/ubuntubase/prebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-basedir=$(dirname "$(readlink -f "$0")")
-
-if [[ ! -f $basedir/authorized_keys && -f ~/.ssh/id_rsa.pub ]]; then
-    cat ~/.ssh/id_rsa.pub >> $basedir/authorized_keys
-fi
