8f746fc1359a711eb668a3bcf2a6ea45521ae37f
gene <gene@nawama.com>
2017-11-28 07:56:28 -0800

    add ubuntu/phoenix-framework/install.sh

--- ubuntu/nodejs/install.sh
+++ ubuntu/nodejs/install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # See https://github.com/nodesource/distributions#installation-instructions
+VER=${1:-8.x}
 
-if [ -z "$1" ]; then
-    curl -sL https://deb.nodesource.com/setup | sudo -E bash -
-else
-    curl -sL https://deb.nodesource.com/setup_$1 | sudo -E bash -
-fi
+curl -sL https://deb.nodesource.com/setup_$VER | sudo -E bash -
+
+# if [ -z "$1" ]; then
+#     curl -sL https://deb.nodesource.com/setup | sudo -E bash -
+# else
+#     curl -sL https://deb.nodesource.com/setup_$1 | sudo -E bash -
+# fi
 
 sudo apt-get install -y nodejs
 
---
+++ ubuntu/phoenix-framework/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
+echo "Visit https://hexdocs.pm/phoenix/installation.html"
+
