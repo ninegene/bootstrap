75c58a5117cc54a88cdd606c7c98e7488dc9b36a
gene <gene@nawama.com>
2017-12-13 00:05:10 -0800

    add script to install posgresql 9.6

---
+++ ubuntu/postgresql/install-9.6-client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+
+sudo apt-get install -y postgresql-client-9.6
