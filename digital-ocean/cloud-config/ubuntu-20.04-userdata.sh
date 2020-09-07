a6144d8b51ca41c62840d81f21a9b3a2d8288296
aung <aung@a2mbp.local>
2020-09-06 19:52:57 -0700

    refactor into user data bash script and add script for wordpress

---
+++ digital-ocean/cloud-config/ubuntu-20.04-userdata.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+
+username="ubuntu"
+ssh_port="4444"
+swap_gb="1"
+swappiness="10"
+timezone="America/Los_Angeles"
+php_upload_max_filesize="25M"
+php_post_max_size="25M"
+php_max_execution_time="120"
+
+add_user() {
+    # Add user with sudo access with no password prompt
+    useradd -m -d /home/${username} -s /bin/bash ${username}
+    echo "${username} ALL=(ALL) NOPASSWD:ALL" | EDITOR='tee -a' visudo
+    usermod -aG sudo ${username}
+    mkdir /home/${username}/.ssh
+    echo "
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCuxfLPQemi9kKPcyhJRAyukMThoX1JZ8oYzukHmAjdeSDbZ+p0hahn93q1QSulLuadmkUOY5gLsjHBHndVMrp8oEKSuGONvDytVARLiek+32WxCK5a6WiZ06DXeBtT30xHgJd2FZW1SdP+ysUdXp22rjOGfrNAYeymVKJwZ2ZI+sTenBLE6TyJ7xxmNRng0b3WkwQbBntXxnh9jFT0MPKd+AAXkDDW/I8hqIEJqyINZAdRbFUIAmzObqjG4otJ5G7rNmbKvzSALrFENHNH8aXrvY3s54+2XJwlDQhtSzdnt6Jx8LTCrGK2ANyn0zcsopLkolwpTZWW9Q0sJKoUISL8qZuqPTGue90e202VO8jEIHjBa4euMBqujEmjQ6BenldYnlaW4DFMIZ3HYNsItfx5KKBI6Z5GApJnpnELf0AVwiVGhP3hvVWLuTbFeSuA3NzYE5AlZREY8ZA42FCX1uOU3IjIiZjIpZMpEh8jJsxiGewCMloYGRfa2tfMr4d/3mKFJ80/JmNTvdVzxCD8+1VCvYqx0wvBuC7QBe38Vh3JC0ffKHGRp44e6jg+eE/FE/c/zVPAoSjC9DjOj1Dmwfz/tsTts8smD/naHFReyfJpj/olxGI7ZtI75fXtuIFG9UnV87FTElAwWdKMJncc5/pcpkDub7wGbMCNvx7G1SrAvQ== aung@ninegene.com
+    " > /home/${username}/.ssh/authorized_keys
+    chown -R ${username}:${username} /home/${username}/.ssh
+    chmod 700 /home/${username}/.ssh
+    chmod 600 /home/${username}/.ssh/authorized_keys
+
+    # Disable root ssh login and change ssh port
+    sed -i -e "/^#Port/s/^.*$/Port ${ssh_port}/" -e "/^Port/s/^.*$/Port ${ssh_port}/" /etc/ssh/sshd_config
+    sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
+    sed -i -e "\$aAllowUsers ${username}" /etc/ssh/sshd_config
+    systemctl restart ssh
+}
+
+install_base_packages() {
+    apt upgrade -y
+    apt install -y tree unzip curl wget jq mlocate unattended-upgrades
+    apt install -y htop nmon netcat net-tools lsof tcpdump mtr bind9-utils openssl
+
+    # config: /etc/fail2ban/jail.conf
+    # log: /var/log/auth.log
+}
+
+config_vim() {
+    apt install -y vim-nox git
+    runuser -l ${username} -c 'mkdir -p ~/.vim/{autoload,bundle}'
+    runuser -l ${username} -c 'curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/bling/vim-airline.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/tpope/vim-repeat.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/tpope/vim-eunuch.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/tpope/vim-endwise.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/tpope/vim-surround.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/tpope/vim-unimpaired.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/tpope/vim-commentary.git'
+    runuser -l ${username} -c 'cd ~/.vim/bundle; git clone --depth=1 https://github.com/scrooloose/syntastic.git'
+    echo '
+execute pathogen#infect()
+execute pathogen#helptags()
+syntax on
+filetype plugin indent on
+set nocompatible
+set history=700
+set syntax=on
+set autoread
+set hlsearch
+set showmatch
+set cursorline
+set scrolloff=999
+set ruler
+set wildmenu
+set laststatus=2
+set wildmode=list:longest
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set smarttab
+set wildmenu
+set wildmode=list:longest,list:full
+set wildignore+=*/tmp/*,*.swp,*.pyc,*.class
+set visualbell
+set hidden
+set switchbuf=useopen
+let mapleader = ","
+let g:mapleader = ","
+nmap <silent> <leader>d "_d
+map <silent> <leader>d "_d
+map <leader>/ :setlocal hlsearch!<CR>:set hlsearch?<CR>
+map <leader>c :setlocal noignorecase!<CR>:set noignorecase?<CR>
+noremap <leader>q :bp <BAR> bd #<CR>
+nnoremap <C-h> "ryiw:%s/<C-r>r//gc<Left><Left><Left>
+vnoremap <C-h> "ryiw:%s/<C-r>r//gc<Left><Left><Left>
+" auto toggle paste
+let &t_SI .= "\<Esc>[?2004h"
+let &t_EI .= "\<Esc>[?2004l"
+inoremap <special> <expr> <Esc>[200~ XTermPasteBegin()
+function! XTermPasteBegin()
+  set pastetoggle=<Esc>[201~
+  set paste
+  return ""
+endfunction
+let g:airline#extensions#branch#enabled = 1
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#syntastic#enabled = 1
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0
+' >> /home/${username}/.vimrc
+    chown ${username}:${username} /home/${username}/.vimrc
+    ln -sf /home/${username}/.vimrc /root/.vimrc
+    ln -sf /home/${username}/.vim /root/.vim
+}
+
+config_screen() {
+    apt install -y screen
+    echo '
+altscreen on
+termcapinfo xterm * ti @: te @
+defbce on
+term xterm-256color
+defscrollback 30000
+hardstatus alwayslastline "%{= kc}%H (load: %l) %-21<%=%D %Y-%m-%d %02c:%s"
+' > /home/${username}/.screenrc
+    chown ${username}:${username} /home/${username}/.screenrc
+}
+
+install_php() {
+    apt install -y --no-install-recommends php php-fpm php-cli php-mysql php-json php-opcache php-mbstring php-xml php-gd php-curl
+    # https://www.nginx.com/resources/wiki/start/topics/tutorials/config_pitfalls/?highlight=pitfalls#passing-uncontrolled-requests-to-php
+    # Bump up max upload size for wordpress file upload
+    sed -i.bak \
+            -e 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' \
+            -e "s/upload_max_filesize = .*/upload_max_filesize = ${php_upload_max_filesize}/" \
+            -e "s/post_max_size = .*/post_max_size = ${php_post_max_size}/" \
+            -e "s/max_execution_time = .*/max_execution_time = ${php_max_execution_time}/" \
+            /etc/php/7.4/fpm/php.ini
+    systemctl restart php7.4-fpm
+}
+
+install_wordpress_cli() {
+    # Install WordPress CLI
+    curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x /usr/local/bin/wp
+    /usr/local/bin/wp --info
+}
+
+install_nginx() {
+    apt install -y nginx certbot python3-certbot-nginx
+}
+
+install_mariadb_server() {
+    # Install MariaDB 10.5
+    apt install -y software-properties-common
+    apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+    add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://mirror.lstn.net/mariadb/repo/10.5/ubuntu $(lsb_release -cs) main"
+    apt update
+
+    apt install -y mariadb-server
+
+    # Enter current password for root (enter for none):
+    # Switch to unix_socket authentication [Y/n] n
+    # Change the root password? [Y/n] n
+    # Remove anonymous users? [Y/n]
+    # Disallow root login remotely? [Y/n]
+    # Remove test database and access to it? [Y/n]
+    # Reload privilege tables now? [Y/n]
+    echo -e "\nn\nn\n\n\n\n\n" | mysql_secure_installation
+
+    # login to root as: sudo mysql -u root
+}
+
+add_swap_space() {
+    fallocate -l ${swap_gb}G /swap.1
+    dd if=/dev/zero of=/swap.1 bs=1M count=$(((${swap_gb} * 1024)))
+    chmod 600 /swap.1
+    mkswap /swap.1
+    swapon /swap.1
+    echo "/swap.1 swap swap defaults 0 0" >> /etc/fstab
+
+    # Use swap when memory is (100 - swappiness) %
+    echo "vm.swappiness = ${swappiness}" >> /etc/sysctl.conf
+    echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+}
+
+config_tcp_sysctl() {
+    # Increase TCP Related Limits
+    echo "net.core.netdev_max_backlog = 30000" >> /etc/sysctl.conf
+    echo "net.ipv4.tcp_max_syn_backlog = 8096" >> /etc/sysctlconf
+    echo "net.core.rmem_max = 134217728" >> /etc/sysctl.conf
+    echo "net.core.wmem_max = 134217728" >> /etc/sysctl.conf
+    echo "net.ipv4.tcp_rmem = 4096 87380 67108864" >> /etc/sysctl.conf
+    echo "net.ipv4.tcp_wmem = 4096 87380 67108864" >> /etc/sysctl.conf
+}
+
+config_ulimit() {
+    # Increase Open Files Limit
+    echo "root soft nofile 4096" >> /etc/security/limits.conf
+    echo "root hard nofile 10240" >> /etc/security/limits.conf
+    echo "${username} soft nofile 4096" >> /etc/security/limits.conf
+    echo "${username} hard nofile 10240" >> /etc/security/limits.conf
+}
+
+set_timezone() {
+    timedatectl set-timezone ${timezone}
+}
+
+cleanup() {
+    apt autoremove -y
+}
+
+### main ###
+
+log_file=/var/log/cloud-init-userdata.log
+
+# log all output to one file and to the screen
+exec > >(tee ${log_file}) 2>&1
+
+start_time=$(date +%s)
+set -x
+
+date "+%F %H:%M:%S %z"
+set_timezone
+date "+%F %H:%M:%S %z"
+
+add_user
+install_base_packages
+config_vim
+config_screen
+install_php
+install_wordpress_cli
+install_nginx
+install_mariadb_server
+add_swap_space
+config_tcp_sysctl
+config_ulimit
+cleanup
+
+set +x
+end_time=$(date +%s)
+
+echo "$(date "+%F %H:%M:%S %z") Done. Time taken: $((${end_time} - ${start_time})) seconds"
+
+sleep 1
+reboot
---
+++ digital-ocean/setup-wordpress-nginx-site.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+set -euo pipefail
+
+[[ $UID -eq 0 ]] && { echo "Require to run as non root user!"; } && exit 1
+
+# Based on:
+# https://ubuntu.com/tutorials/install-and-configure-wordpress#4-configure-database
+# https://www.digitalocean.com/community/tutorials/how-to-install-wordpress-with-lemp-on-ubuntu-18-04
+
+if [[ $# -ne 7 ]]; then
+    echo "
+    Usage:
+    $0 <domain> <dbname> <dbuser> <dbpass> <wpuser> <wppass> <wpemail>
+    "
+    exit 1
+fi
+
+domain=$1
+dbname=$2
+dbuser=$3
+dbpass=$4
+wpuser=$5
+wppass=$6
+wpemail=$7
+wp_dir="/var/www/${domain}/wordpress"
+
+if [[ -d ${wp_dir} ]]; then
+    echo "${wp_dir} exists!"
+    exit 1
+fi
+
+set +e
+sudo mysql -u root -e "CREATE DATABASE ${dbname} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;;"
+
+sudo mysql -u root -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER \
+    ON ${dbname}.* TO ${dbuser}@localhost \
+    IDENTIFIED BY '${dbpass}'; \
+    FLUSH PRIVILEGES;"
+set -e
+
+sudo mkdir -p ${wp_dir}
+sudo chown -R $USER:www-data ${wp_dir}/..
+
+cd ${wp_dir}
+
+# download wordpress
+wp core download
+
+# generate wp-config.php
+# https://developer.wordpress.org/cli/commands/config/create/
+wp config create --dbname=${dbname} --dbuser=${dbuser} --dbpass=${dbpass} --path=${wp_dir}
+
+#echo "define('FS_METHOD', 'direct');" >> ${wp_dir}/wp-config.php
+
+# https://www.nginx.com/blog/installing-wordpress-with-nginx-unit/
+ls -lF ${wp_dir}
+sudo chown -R $USER:www-data ${wp_dir}
+sudo find ${wp_dir} -type d -exec chmod g+s {} \;
+sudo chmod g+w ${wp_dir}/wp-content
+sudo chmod -R g+w ${wp_dir}/wp-content/themes
+sudo chmod -R g+w ${wp_dir}/wp-content/plugins
+
+# Remove other access
+sudo chmod 640 ${wp_dir}/wp-config.php
+#sudo find ${wp_dir} -type d -exec chmod 750 {} \;
+#sudo find ${wp_dir} -type f -exec chmod 640 {} \;
+
+if [[ -f /etc/nginx/sites-available/${domain}.conf ]]; then
+    echo "File exists: /etc/nginx/sites-available/${domain}.conf"
+    read -p "Overwrite? [Y/n]" confirm
+    if [[ ${confirm} == "n" ]]; then
+        exit 1
+    fi
+fi
+
+echo "
+server {
+    listen 80;
+    listen [::]:80;
+
+    server_name    ${domain};
+
+    root           ${wp_dir};
+    index          index.php index.html;
+
+    location / {
+        try_files \$uri \$uri/ /index.php;
+    }
+
+    location ~ \.php$ {
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+    }
+
+    access_log /var/log/nginx/${domain}.access.log;
+    error_log /var/log/nginx/${domain}.error.log warn;
+}
+" | sudo tee /etc/nginx/sites-available/${domain}.conf
+
+set -x
+sudo ln -sf /etc/nginx/sites-available/${domain}.conf /etc/nginx/sites-enabled/${domain}.conf
+
+# Check syntax
+sudo nginx -t
+sudo systemctl restart nginx
+
+# Generalte Cert
+sudo certbot -d springsprout.com --agree-tos -m certbot@ninegene.com --nginx --redirect -n
+sudo systemctl reload nginx
+
+set -x
+# https://developer.wordpress.org/cli/commands/core/install/
+#wp core install --url="${domain}" \
+#    --title="${domain}" \
+#    --admin_user="${wpuser}" \
+#    --admin_password="${wppass}" \
+#    --admin_email="${wpemail}"
+#
