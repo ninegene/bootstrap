eb09ab0db370439f00ac6a208c3fdd74d121163a
alo <aungloo@gmail.com>
2025-09-14 18:00:10 -0700

    feat: add configure-git-config.sh

---
+++ ubuntu-24.04/configure-git-config.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+git config --global core.editor vim
+
+git config --global alias.aliases "!git config --get-regexp 'alias.*' | colrm 1 6 | sed 's/[ ]/ = /'"
+git config --global alias.me "!git config user.name && git config user.email"
+git config --global alias.user "config user.name"
+git config --global alias.email "config user.email"
+
+git config --global alias.st "status --short --branch"
+git config --global alias.br "branch"
+
+git config --global alias.co "checkout"
+git config --global alias.cob "checkout -b"
+
+git config --global alias.unstage 'reset HEAD --'
+
+git config --global alias.aa "add --all"
+
+git config --global alias.ci "commit -m"
+git config --global alias.amend "commit --amend"
+
+git config --global alias.d "diff"
+git config --global alias.ds "diff --staged" # Same as "diff --cached"
+
+git config --global alias.dw "diff --word-diff=color"
+git config --global alias.dsw "diff --word-diff=color --staged"
+
+# Show last commit
+git config --global alias.last '!git ll -1 HEAD'
+
+# Show commits in one per line (abbrev commit, short date, tag, message, author)
+git config --global alias.ls "log --abbrev-commit --date=short --pretty=format:'%C(yellow)%h %ad%Creset%Cred%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+git config --global alias.ll "!git ls --name-status"
+
+# concise with graph
+git config --global alias.lg "log --oneline --decorate --all --graph"
+
+# Show new commits created by the last command (typically after a 'git pull')
+# Example:
+# $ git changes
+# $ git changes origin/master
+git config --global alias.changes "!sh -c 'git ll $1@{1}..$1@{0} $*'"
+
+git config --global alias.pr "!git pull -v --rebase && git changes"
+
+# Show all commits since version v2.2.11 that changed any file in the 'src' or 'test' subdirectories
+# (Use 'git tag' to see all version tag)
+# $ git log v2.2.11.. src test
+
+# Show the commits that are in the "dev" branch but not yet in the 'master' branch,
+# along with the list of paths each commit modifies.
+# $ git log --name-status master..dev
+# $ git ll master..dev
+
+# Show the commits that changed 'config/application.properties', including those commits that
+# occurred before the file was given its present name. (Show file commit history)
+# $ git f application.properties
+git config --global alias.fc "!git ls --follow"
+
+# Show file commit history with diffs
+git config --global alias.fh "!git ls -u"
+
+# Show commits in local timezone
+git config --global alias.locallog "log --date=local"
+
+# Case insensitive search text
+git config --global alias.grep "grep -i"
+
+git config --global alias.grepfiles "!git ls-files | grep -i"
+
