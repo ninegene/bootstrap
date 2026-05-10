58e47b0ece3d5da623043ff261b22a35522c9fe0
ALO <1923005+ninegene@users.noreply.github.com>
2026-05-10 15:53:00 -0700

    fix(macOS): update configure-zsh.sh to correctly manage zsh_aliases in .zshrc

--- macOS/configure-zsh.sh
+++ macOS/configure-zsh.sh
@@ -2,11 +2,16 @@
 set -eo pipefail
 
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+ZSH_ALIASES_SOURCE="source $REPO_ROOT/macOS/zsh_aliases"
 
 echo "Configuring Zsh..."
-if ! grep -q "^source $REPO_ROOT/macOS/zsh_aliases" ~/.zshrc; then
+if [ -f ~/.zshrc ]; then
+    perl -0pi -e 's|^source .*/bootstrap/macOS/zsh_aliases$||mg' ~/.zshrc
+fi
+
+if ! grep -Fxq "$ZSH_ALIASES_SOURCE" ~/.zshrc; then
     echo "Updating ~/.zshrc to load zsh_aliases..."
-    echo "source $REPO_ROOT/macOS/zsh_aliases" >>~/.zshrc
+    echo "$ZSH_ALIASES_SOURCE" >>~/.zshrc
 fi
 
 echo "Configuring .screenrc file..."
