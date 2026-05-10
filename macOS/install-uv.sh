a63f214e6863fa76ad022d3b820fec4d828d3ab7
ALO <1923005+ninegene@users.noreply.github.com>
2026-05-10 15:53:41 -0700

    fix(macOS): update install-uv.sh to disable additional shellcheck warnings

--- macOS/install-uv.sh
+++ macOS/install-uv.sh
@@ -9,7 +9,7 @@ fi
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 export PATH="$HOME/.local/bin:$PATH"
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC1091
 [ -f "$HOME/.local/bin/env" ] && . "$HOME/.local/bin/env"
 
 set -x
